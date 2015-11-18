264171b354416e0891c41a9c4f9c71ec7d265678
Francesc Leveque <francesc@leveque.es>
2015-11-18 11:56:00 +0100

    add bundler aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,11 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+# bundler
+alias b='bundle'
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
+alias rr='bundle exec rake routes'
+alias rt='bundle exec rake -T --all'

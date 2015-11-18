4e129489fd4aa93dc3d5464160a44ef698cc36d1
Francesc Leveque <francesc@leveque.es>
2015-11-18 12:02:14 +0100

    rails aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,6 +2,15 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
+alias rb='ruby'
+alias rs='rails s'
+alias rc='rails c'
+alias rg='rails g'
+alias rd='rails d'
+
+alias gemi='gem install --no-ri --no-rdoc'
+alias gemu='gem uninstall'
+
 # bundler
 alias b='bundle'
 alias be='bundle exec'
